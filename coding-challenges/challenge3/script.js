e7c819273b1068b965e05a99d6e8fef2397961cd
Ishan <its.ishan.kumar@gmail.com>
2019-05-20 11:32:59 +0530

    updated for nested array.

--- coding-challenges/challenge3/script.js
+++ coding-challenges/challenge3/script.js
@@ -1,15 +1,17 @@
 function compare (arr1, arr2) {
     var result;
-    if (arr1.length != arr2.length)
-        result = false;
-    else
-        for (var i=0;i<arr1.length;i++) {
-            if (arr1[i].length > 1) 
-                compare (arr1[i], arr2[i]);
-            else if (arr1[i] !== arr2[i])
-                result = false;
-            else
+      if (arr1.length != arr2.length)
+          return false;
+      else
+          for (var i=0;i<arr1.length;i++) {
+              if (arr1[i].length > 1 ) 
+                  return compare(arr1[i], arr2[i]);
+              else if (arr1[i] !== arr2[i])
+                  return false;
+              else
                 result = true;
-        }
-    return result;
-}
+          }
+      return result
+  }
+  
+  compare([1, [3,2]],[1, [3,2]])

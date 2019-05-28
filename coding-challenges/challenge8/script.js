9f92356d8d18260c9aa163d96c94618b6f6c108c
Ishan <its.ishan.kumar@gmail.com>
2019-05-28 11:26:29 +0530

    reset added

--- coding-challenges/challenge8/script.js
+++ coding-challenges/challenge8/script.js
@@ -16,6 +16,7 @@ function search() {
     function renderTable(books) {
         console.log(books[0])
         let root = document.getElementsByTagName('tbody')[0]
+        root.innerHTML = ''; 
         for (book in books) {
             // console.log(books[book])
             let row = []

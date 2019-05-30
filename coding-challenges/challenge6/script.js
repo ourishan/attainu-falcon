e9aefc9556838004cddd9df0b04922d2e89c2333
Ishan <its.ishan.kumar@gmail.com>
2019-05-31 05:06:49 +0530

    updated challenge6.

--- coding-challenges/challenge6/script.js
+++ coding-challenges/challenge6/script.js
@@ -1,14 +1,20 @@
-
-function checkCities(ch) {
-    function check(data) {
-      for (i = 0; i < data.length; i++) {
-        if (data[i].name[0].toLowerCase() == ch.toLowerCase())
-          console.log(data[i].name);
-      }
-    }
-    fetch("cities.json")
-      .then((arr) => arr.json())
-      .then(check)
-      .catch(err => err);
+function check(ch, data) {
+  let cities = []
+  for (i = 0; i < data.length; i++) {
+    if (data[i].name[0].toLowerCase() == ch.toLowerCase())
+      cities.push(data[i].name);
   }
-  console.log(checkCities('e'));
+  return cities
+}
+
+async function checkCities(ch) {
+
+  let cts = await fetch("cities.json")
+  let j = await cts.json()
+  let sc = await check(ch, j)
+  return sc
+}
+
+(async () => {
+  console.log(await checkCities('i'))
+})()

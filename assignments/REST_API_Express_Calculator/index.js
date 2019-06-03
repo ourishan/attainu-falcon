247a45e76d4acfca0f5cf8af5a530a7f9fdd448c
Ishan <its.ishan.kumar@gmail.com>
2019-06-03 12:09:29 +0530

    updated rest_api_express_calculator.

---
+++ assignments/REST_API_Express_Calculator/index.js
@@ -0,0 +1,30 @@
+var express = require('express')
+var app = express()
+
+app.use(express.urlencoded())
+
+app.post('/add', function (req, res) {
+    let num1 = parseInt(req.body.num1)
+    let num2 = parseInt(req.body.num2)
+    let result = num1 + num2
+    res.send({num1, num2, result})
+})
+app.post('/sub', function (req, res) {
+    let num1 = parseInt(req.body.num1)
+    let num2 = parseInt(req.body.num2)
+    let result = num1 - num2
+    res.send({num1, num2, result})
+})
+app.post('/mul', function (req, res) {
+    let num1 = parseInt(req.body.num1)
+    let num2 = parseInt(req.body.num2)
+    let result = num1 * num2
+    res.send({num1, num2, result})
+})
+app.post('/div', function (req, res) {
+    let num1 = parseInt(req.body.num1)
+    let num2 = parseInt(req.body.num2)
+    let result = num1 / num2
+    res.send({num1, num2, result})
+})
+app.listen(3000, ()=> console.log(`listening on port 3000!`))

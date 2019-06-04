d679f49db27fde429a13c8e686731f3c56f47ddc
Ishan <its.ishan.kumar@gmail.com>
2019-06-04 09:47:50 +0530

    used Math.

--- coding-challenges/challenge13/script.js
+++ coding-challenges/challenge13/script.js
@@ -1,6 +1,5 @@
 function palin(str) {
-    let rng = str.length % 2 ? (str.length - 1) / 2 : str.length / 2
-    for (let i = 0; i < rng; i++) {
+    for (let i = 0; i < Math.floor(str.length/2); i++) {
         if (str[i].toLowerCase() != str[str.length - 1 - i].toLowerCase()) {
             console.log("not a pallindrome")
             return false
@@ -9,4 +8,4 @@ function palin(str) {
     console.log("pallindrome!")
     return true
 }
-palin("kyak")
+palin("kyyayyk")

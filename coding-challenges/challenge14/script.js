37bbbf1e36aad8bfa4032859a51b673d1230ce73
Ishan <its.ishan.kumar@gmail.com>
2019-06-05 09:38:02 +0530

    updated challenge14.

---
+++ coding-challenges/challenge14/script.js
@@ -0,0 +1,10 @@
+function toHours(dur) {
+    if(dur<60) {
+        return 0+':'+dur
+    }
+    let sec = dur%60
+    let min  = Math.floor(dur/60)%60
+    let hrs  = Math.floor(dur/3600)   
+    return hrs+':'+min%60+':'+sec
+}
+console.log(toHours(3920))

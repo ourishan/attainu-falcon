ae79899dbf60b23d9ef9a85cd26378e1528b2a82
Ishan <its.ishan.kumar@gmail.com>
2019-06-07 12:39:01 +0530

    updated challenge16.

---
+++ coding-challenges/challenge16/script.js
@@ -0,0 +1,6 @@
+function params(url) {
+    let num1 = parseFloat(url.substring(url.indexOf('=')+1,url.indexOf('&')))
+    let num2 = parseFloat(url.substring(url.lastIndexOf('=')+1,url.length))
+    return num1 && num2 ? {"\"num1\"": num1, "\"num2\"": num2} : "Missing URL parameters!"
+}
+console.log(params("http://localhost:3000/add"))

305fcb15d03eb232ac8fdf6d0e2f5fbe4c29bae1
Ishan <its.ishan.kumar@gmail.com>
2019-06-11 10:07:43 +0530

    take care of exceptions.

--- coding-challenges/challenge18/script.js
+++ coding-challenges/challenge18/script.js
@@ -2,12 +2,12 @@ function decToBin(dec) {
     let bin = 0,
         div = dec,
         unit = 1
-    while (div !== 1) {
+    while (div > 1) {
         bin += (div % 2) * unit
         div = Math.floor(div / 2)
         unit = unit * 10
     }
-    bin = unit + bin
+    bin = div*unit + bin
     return bin
 }
-console.log(decToBin(67))
+console.log(decToBin(7))

f9746a3b66a3c9302bc1ea11194c4caa25c3685f
Ishan <its.ishan.kumar@gmail.com>
2019-06-12 19:27:14 +0530

    opimizations

--- coding-challenges/challenge19/script.js
+++ coding-challenges/challenge19/script.js
@@ -1,6 +1,6 @@
-function fib(iteration) {
-    while (iteration !== 2) {
-        return [...fib(iteration-1),fib(iteration-1)[fib(iteration-1).length-1]+fib(iteration-1)[fib(iteration-1).length-2]]
+function fib(i) {
+    while (i !== 2) {
+        return [...fib(i-1),fib(i-1)[i-2]+fib(i-1)[i-3]]
     }
     return [1,1]
 }

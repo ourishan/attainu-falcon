0b207e53bb5d60ff8674538a7c398ef097635190
Ishan <its.ishan.kumar@gmail.com>
2019-07-17 09:30:02 +0530

    challenge21

---
+++ coding-challenges/challenge21/script.js
@@ -0,0 +1,11 @@
+function inWords(digits) {
+    let literals = digits.toString()
+    wordsList = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"]
+    let words = ""
+    for (let index = 0; index < literals.length; index++) {
+        const digit = parseInt(literals[index])
+        words += wordsList[digit] + " "
+    }
+    return words
+}
+console.log(inWords(109))

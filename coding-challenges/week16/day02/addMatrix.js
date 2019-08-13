874969f6357b6cb1b8d1db838250809f81aca715
Ishan <its.ishan.kumar@gmail.com>
2019-08-13 11:05:34 +0530

    implicit array

--- coding-challenges/week16/day02/addMatrix.js
+++ coding-challenges/week16/day02/addMatrix.js
@@ -7,5 +7,5 @@ var b = [[1, 1, 1, 1],
 [2, 2, 2, 2],
 [3, 3, 3, 3],
 [4, 4, 4, 4]];
-let addMatrix = (a, b) => Array.isArray(a) ? a.map((o, i) => addMatrix(a[i], b[i])) : a + b
+let addMatrix = (a, b) => a.map((o, i) => Array.isArray(o) ? addMatrix(a[i], b[i]) : a[i] + b[i])
 console.log(addMatrix(a, b))
